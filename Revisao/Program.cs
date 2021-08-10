da70913e536514121c0e6b5ddf8e35efaec48a0f
DiegoGuse <Diegofguse@gmail.com>
2021-08-10 11:46:16 -0300

    Revisao do curso
    Projeto final do curso

---
+++ Revisao/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace Revisao
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Informe a opção desejada :");
+            Console.WriteLine("1- Inserir novo aluno");
+            Console.WriteLine("2- Listar alunos");
+            Console.WriteLine("3- Calcular média geral");
+            Console.WriteLine("X- Sair");
+            Console.WriteLine();
+
+
+            string opcaoUsuario = Console.ReadLine();
+
+            while (opcaoUsuario.ToUpper() != "X")
+            {
+                switch(opcaoUsuario)
+                {
+                    case "1":
+                            //todo: adicionar aluno
+                        break;
+                    case "2":
+                            //todo: listar alunos
+                        break;
+                    case "3":
+                        //todo: calcular media geral 
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                }
+
+                    Console.WriteLine("Informe a opção desejada :");
+                    Console.WriteLine("1- Inserir novo aluno");
+                    Console.WriteLine("2- Listar alunos");
+                    Console.WriteLine("3- Calcular média geral");
+                    Console.WriteLine("X- Sair");
+                    Console.WriteLine();
+
+                    opcaoUsuario = Console.ReadLine();
+            }
+
+        }
+    }
+}
